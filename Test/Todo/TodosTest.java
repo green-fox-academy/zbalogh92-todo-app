d3aa018a68abb77e9bd9f8d09ac44071d2e57e54
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-04-07 12:25:34 +0200

    create tests for Todos class

--- Test/Todo/TodosTest.java
+++ Test/Todo/TodosTest.java
@@ -1,5 +1,8 @@
 package Todo;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -7,4 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class TodosTest {
 
+  @Test
+  void testGetSize() {
+    Todos helper = new Todos();
+    helper.addToList("testString1");
+    helper.addToList("testString2");
+    assertEquals(2, helper.getSize());
+  }
 }
